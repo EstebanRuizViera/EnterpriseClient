16206be888db09320cac1c5eb7e6dff966b1151f
Esteban <nutrete.de.educacion@gmail.com>
2020-02-05 19:18:42 +0000

    finish local database user

--- app/src/main/java/com/example/enterpriseclient/SynchronizeThread.kt
+++ app/src/main/java/com/example/enterpriseclient/SynchronizeThread.kt
@@ -1,8 +1,24 @@
 package com.example.enterpriseclient
 
+import android.app.Activity
+import android.widget.TextView
+import com.example.enterpriseclient.myDataBase.model.Product
+import com.example.enterpriseclient.requestServer.RequestProduct
+
+
 class SynchronizeThread: Runnable {
 
+    private var activity: Activity
+
+    constructor(activity: Activity) {
+        this.activity = activity
+    }
+
     override fun run() {
-        //codigo para sincronizar las tablas de la base de datos remota con la local
+
+    }
+
+    private fun sincronizedProduct(){
+        var productList = RequestProduct.selectAllProducts(activity.applicationContext)
     }
 }
